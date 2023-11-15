a8b1e2358688eea1f67038df558c486fc5b6fe4f
mohamedhosnyfoad <mohamedhosnyfoad@gmail.com>
2023-11-15 09:26:25 -0800

    team

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _printf - is a function that selects the correct function to print.
  * @format: identifier to look for.
@@ -8,16 +7,19 @@
 int _printf(const char * const format, ...)
 {
 	convert p[] = {
-		{"%s", print_s}, {"%c", print_char},
+		{"%s", print_s}, {"%c", print_c},
 		{"%%", print_37},
-		{"%i", print_integer}, {"%d", print_dec}, {"%r", print_revs},
+		{"%i", print_i}, {"%d", print_d}, {"%r", print_revs},
+		{"%R", print_rot13}, {"%b", print_bin},
 		{"%u", print_unsigned},
+		{"%o", print_oct}, {"%x", print_hex}, {"%X", print_HEX},
+		{"%S", print_exc_string}, {"%p", print_pointer}
 	};
 
-	va_list list;
+	va_list args;
 	int i = 0, j, length = 0;
 
-	va_start(list, format);
+	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
@@ -29,7 +31,7 @@ Here:
 		{
 			if (p[j].ph[0] == format[i] && p[j].ph[1] == format[i + 1])
 			{
-				length += p[j].function(list);
+				length += p[j].function(args);
 				i = i + 2;
 				goto Here;
 			}
@@ -39,6 +41,6 @@ Here:
 		length++;
 		i++;
 	}
-	va_end(list);
+	va_end(args);
 	return (length);
 }
--- main.h
+++ main.h
@@ -21,20 +21,27 @@ typedef struct format
 	int (*function)();
 } convert;
 
-/*putchar prototype */
-
-int _putchar(char c);
-
-/*prints characters */
-int print_char(va_list list);
-int print_string(va_list list);
-int _strlen(char *s);
+int *_strcpy(char *dest, char *src);
+int print_pointer(va_list val);
+int print_unsigned(va_list args);
+int print_hex_extra(unsigned long int num);
 int _strlenc(const char *s);
+int print_HEX_extra(unsigned int num);
+int print_exc_string(va_list val);
+int print_HEX(va_list val);
+int print_hex(va_list val);
+int print_oct(va_list val);
+int print_bin(va_list val);
+int print_revs(va_list args);
+int print_rot13(va_list args);
+int print_i(va_list args);
+int print_d(va_list args);
+int _strlen(char *s);
+int rev_string(char *s);
 int print_37(void);
-
-int print_integer(va_list list);
-int print_dec(va_list list);
-
+int print_c(va_list val);
+int print_s(va_list val);
+int _putchar(char c);
 int _printf(const char *format, ...);
 
 #endif
