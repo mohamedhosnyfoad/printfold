e513c3902fbd7ef6607524e029a4f4eafc1855d6
mohamedhosnyfoad <mohamedhosnyfoad@gmail.com>
2023-11-16 16:54:34 -0800

    0x11

--- _printf.c
+++ _printf.c
@@ -6,41 +6,40 @@
  */
 int _printf(const char * const format, ...)
 {
-	convert p[] = {
-		{"%s", print_s}, {"%c", print_c},
-		{"%%", print_37},
-		{"%i", print_i}, {"%d", print_d}, {"%r", print_revs},
-		{"%R", print_rot13}, {"%b", print_bin},
-		{"%u", print_unsigned},
-		{"%o", print_oct}, {"%x", print_hex}, {"%X", print_HEX},
-		{"%S", print_exc_string}, {"%p", print_pointer}
-	};
+convert_match m[] = {
+{"%s", print_string}, {"%c", print_character},
+{"%%", print_percent},
+{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
+{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+};
 
-	va_list args;
-	int i = 0, j, length = 0;
+va_list args;
+int i = 0, j, len = 0;
 
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
+va_start(args, format);
+if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+return (-1);
 
 Here:
-	while (format[i] != '\0')
-	{
-		j = 13;
-		while (j >= 0)
-		{
-			if (p[j].ph[0] == format[i] && p[j].ph[1] == format[i + 1])
-			{
-				length += p[j].function(args);
-				i = i + 2;
-				goto Here;
-			}
-			j--;
-		}
-		_putchar(format[i]);
-		length++;
-		i++;
-	}
-	va_end(args);
-	return (length);
+while (format[i] != '\0')
+{
+j = 13;
+while (j >= 0)
+{
+if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+{
+len += m[j].f(args);
+i = i + 2;
+goto Here;
+}
+j--;
+}
+_putchar(format[i]);
+len++;
+i++;
+}
+va_end(args);
+return (len);
 }
--- print_string.c
+++ print_string.c
@@ -1,29 +1,28 @@
 #include "main.h"
 /**
- * print_s - print a string.
- * @val: argumen t.
- * Return: the length of the string.
+ * print_string - a function that prints a string.
+ * @val: function parameter.
+ * Return: len.
  */
 
-int print_s(va_list val)
+int print_string(va_list val)
 {
-	char *s;
-	int i, len;
-
-	s = va_arg(val, char *);
-	if (s == NULL)
-	{
-		s = "(null)";
-		len = _strlen(s);
-		for (i = 0; i < len; i++)
-			_putchar(s[i]);
-		return (len);
-	}
-	else
-	{
-		len = _strlen(s);
-		for (i = 0; i < len; i++)
-			_putchar(s[i]);
-		return (len);
-	}
+char *x;
+int i, length;
+x = va_arg(val, char *);
+if (x == NULL)
+{
+x = "(null)";
+length = _strlen(x);
+for (i = 0; i < length; i++)
+_putchar(x[i]);
+return (length);
+}
+else
+{
+length = _strlen(x);
+for (i = 0; i < length; i++)
+_putchar(x[i]);
+return (length);
+}
 }
